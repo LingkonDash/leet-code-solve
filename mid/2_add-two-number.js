fcd2f242b0b403ede34fec28404a1508a0f7bef3
LingkonDash <cocrupon@gmail.com>
2026-07-20 16:36:16 +0600

    solved 2nd problem

--- mid/2_add-two-number.js
+++ mid/2_add-two-number.js
@@ -3,87 +3,20 @@ function ListNode(val, next) {
     this.next = (next === undefined ? null : next);
 }
 
+// l1: [9,9,9,9,9,9,9]
 const l1 = new ListNode(
-    1,
+    9,
     new ListNode(
-        0,
+        9,
         new ListNode(
-            0,
+            9,
             new ListNode(
-                0,
+                9,
                 new ListNode(
-                    0,
+                    9,
                     new ListNode(
-                        0,
-                        new ListNode(
-                            0,
-                            new ListNode(
-                                0,
-                                new ListNode(
-                                    0,
-                                    new ListNode(
-                                        0,
-                                        new ListNode(
-                                            0,
-                                            new ListNode(
-                                                0,
-                                                new ListNode(
-                                                    0,
-                                                    new ListNode(
-                                                        0,
-                                                        new ListNode(
-                                                            0,
-                                                            new ListNode(
-                                                                0,
-                                                                new ListNode(
-                                                                    0,
-                                                                    new ListNode(
-                                                                        0,
-                                                                        new ListNode(
-                                                                            0,
-                                                                            new ListNode(
-                                                                                0,
-                                                                                new ListNode(
-                                                                                    0,
-                                                                                    new ListNode(
-                                                                                        0,
-                                                                                        new ListNode(
-                                                                                            0,
-                                                                                            new ListNode(
-                                                                                                0,
-                                                                                                new ListNode(
-                                                                                                    0,
-                                                                                                    new ListNode(
-                                                                                                        0,
-                                                                                                        new ListNode(
-                                                                                                            0,
-                                                                                                            new ListNode(
-                                                                                                                0,
-                                                                                                                new ListNode(
-                                                                                                                    1
-                                                                                                                )
-                                                                                                            )
-                                                                                                        )
-                                                                                                    )
-                                                                                                )
-                                                                                            )
-                                                                                        )
-                                                                                    )
-                                                                                )
-                                                                            )
-                                                                        )
-                                                                    )
-                                                                )
-                                                            )
-                                                        )
-                                                    )
-                                                )
-                                            )
-                                        )
-                                    )
-                                )
-                            )
-                        )
+                        9,
+                        new ListNode(9)
                     )
                 )
             )
@@ -91,14 +24,18 @@ const l1 = new ListNode(
     )
 );
 
+// l2: [9,9,9,9]
 const l2 = new ListNode(
-    5,
+    9,
     new ListNode(
-        6,
-        new ListNode(4)
+        9,
+        new ListNode(
+            9,
+            new ListNode(9)
+        )
     )
 );
-
+// first approach
 
 /**
  * Definition for singly-linked list.
@@ -112,7 +49,7 @@ const l2 = new ListNode(
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var addTwoNumbers = function (l1, l2) {
+var addTwoNumbersFIRSTapproach = function(l1, l2) {
     let num1 = "";
     let num2 = "";
 
@@ -145,6 +82,65 @@ var addTwoNumbers = function (l1, l2) {
     }
 
     return head.next;
+};
+
+// second approach
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let currentL1 = l1;
+    let currentL2 = l2;
+    let rest = 0;
+    let head = {val: 'dummy', next: null}
+    const result = head
+    while (currentL1 || currentL2 || rest !== 0) {
+
+        if(!currentL1 && !currentL2) {
+            head.next = new ListNode(rest);
+            break;
+        }
+        
+        let sum = currentL1 && currentL2 ? currentL1.val + currentL2.val : currentL1 ? currentL1.val : currentL2.val;
+        
+        if(rest !== 0) sum = sum + rest;
+        
+        if(sum > 9) {
+            
+            rest = Number(`${sum}`[0])
+            sum = Number(`${sum}`[1])
+        } else rest = 0;
+        
+        if(!currentL1) {
+            head.next = new ListNode(sum, null)
+            head = head.next
+            currentL2 = currentL2.next
+            continue;
+        }
+
+        if(!currentL2) {
+            head.next = new ListNode(sum, null)
+            head = head.next
+            currentL1 = currentL1.next;
+            continue;
+        }
+
+        head.next = new ListNode(sum, null)
+        head = head.next
+        currentL1 = currentL1.next;
+        currentL2 = currentL2.next;
+    }
+    return result.next
 
 };
 
@@ -153,6 +149,7 @@ var addTwoNumbers = function (l1, l2) {
 
 const result = addTwoNumbers(l1, l2)
 
+// just to make the output more readable, it will print the linked list as an array
 function printShit(head) {
     const arr = [];
 
